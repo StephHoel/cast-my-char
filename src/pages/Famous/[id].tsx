3b753a06cde2e2def9ff593244d8bc7102b1849b
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-07-22 08:38:56 -0300

    fix: :bug: improve sizes to others screens

--- src/pages/Famous/[id].tsx
+++ src/pages/Famous/[id].tsx
@@ -12,17 +12,24 @@ export function Famous() {
   const { actors, loading } = useActors()
 
   const actor = actors.find((a) => a.id === id)
-  const image = actor && actor.photoUrl.short.trim().length > 0 ? actor.photoUrl.short : placeholder
+  const image =
+    actor && actor.photoUrl.short.trim().length > 0
+      ? actor.photoUrl.short
+      : placeholder
 
   if (loading) return <Loading />
 
   if (!actor) return <p>Famoso ou Famosa não encontrado.</p>
 
   return (
-    <div>
-      <div className='lg:max-w-10/12 flex max-h-[90vh] w-full flex-col overflow-hidden rounded-lg bg-gray-900 text-white shadow-lg'>
-        <div className='relative shrink-0'>
-          <img src={image} alt={actor.name} className='h-64 w-full rounded-t-lg object-cover' />
+    <div className='2xl:w-[50vw] lg:w-[60vw] md:w-[90vw] h-[90vh] flex items-center justify-center'>
+      <div className='w-full h-full mx-auto pb-8 flex flex-col overflow-hidden rounded-lg bg-gray-900 text-white shadow-lg'>
+        <div className='not-md:h-1/4 md:h-1/2 relative shrink-0'>
+          <img
+            src={image}
+            alt={actor.name}
+            className='size-full rounded-t-lg object-cover'
+          />
 
           <a
             href={url.index}
@@ -37,17 +44,29 @@ export function Famous() {
         <div className='space-y-3 overflow-y-auto p-4 text-2xl'>
           <h2 className='text-center text-4xl font-bold'>{actor.name}</h2>
 
-          <Content title='Nacionalidade'>{formatArray(actor.nationality)}</Content>
+          <Content title='Nacionalidade'>
+            {formatArray(actor.nationality) || 'N/A'}
+          </Content>
 
-          <Content title='Cor dos olhos'>{capitalize(actor.eyeColor)}</Content>
+          <Content title='Cor dos olhos'>
+            {capitalize(actor.eyeColor) || 'N/A'}
+          </Content>
 
-          <Content title='Cor do cabelo'>{formatArray(actor.hairColor)}</Content>
+          <Content title='Cor do cabelo'>
+            {formatArray(actor.hairColor) || 'N/A'}
+          </Content>
 
-          <Content title='Faixa etária'>{formatArray(actor.ageRange)}</Content>
+          <Content title='Faixa etária'>
+            {formatArray(actor.ageRange) || 'N/A'}
+          </Content>
 
-          <Content title='Gênero'>{formatArray(actor.gender)}</Content>
+          <Content title='Gênero'>{formatArray(actor.gender) || 'N/A'}</Content>
 
-          {actor.tags?.length > 0 && <Content title='Outras Características'>{formatArray(actor.tags)}</Content>}
+          {actor.tags?.length > 0 && (
+            <Content title='Outras Características'>
+              {formatArray(actor.tags) || 'N/A'}
+            </Content>
+          )}
         </div>
       </div>
     </div>
